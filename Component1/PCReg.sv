8845177824903d809ee6a2090c7502bc24e4fff9
daisy21107 <147391924+daisy21107@users.noreply.github.com>
2023-11-20 15:58:05 +0000

    Component 1
    PCReg.sv works as the top file.
    ProgramCounter.sv is basically the adder(or the MUX in the top-level block diagram)

---
+++ Component1/PCReg.sv
@@ -0,0 +1,19 @@
+module PCReg #(         //topfile
+    parameter       DATA_WIDTH = 4
+) (
+    input logic                     clk,               
+    input logic                     rst,
+    input logic [DATA_WIDTH-1:0]    PCsrc,         
+    output logic [DATA_WIDTH-1:0]   PC   
+);
+    logic [DATA_WIDTH-1:0]   next_PC,
+
+
+ProgramCounter pc1(
+    .clk(clk)
+    .rst(rst)
+    .PCsrc(Pcsrc)
+    .PC(next_PC)
+);
+
+endmodule
---
+++ Component1/ProgramCounter.sv
@@ -0,0 +1,18 @@
+module ProgramCounter #(            //works as an adder
+    parameter DATA_WIDTH = 4
+)(
+    input logic                         clk,
+    input logic                         rst,         
+    input logic                         PCsrc, 
+    input logic [DATA_WIDTH-1:0]        ImmOp, 
+    output logic [DATA_WIDTH-1:0]       PC    
+);
+
+
+always_ff @(posedge clk, posedge rst, posedge PCsrc) begin     //asynchronous
+    if (rst)            PC <= 4'b0;
+    if (PCsrc)          PC <= PC + ImmOp
+    else                PC <= PC + 4'b0100;  
+end
+
+endmodule
